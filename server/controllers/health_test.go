aebc6fd97870053f5803f931dff23b0838f56ec7
R.Sriram <r.sriram@games24x7.com>
2023-08-15 15:43:24 +0530

    added controller unit tests

---
+++ server/controllers/health_test.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gitub.com/sriramr98/hephaestus/mocks"
+)
+
+func TestHealthController(t *testing.T) {
+	t.Run("Returns 200 status code", func(t *testing.T) {
+		res := *httptest.NewRecorder()
+		gc := mocks.MockGin{
+			ResponseRecorder: res,
+		}
+		context := gc.GetTestGinContext()
+
+		GetHealth(context)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+
+	})
+}
